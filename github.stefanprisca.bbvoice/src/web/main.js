8553236c720a2d6960aef1ad8036030ec3d86ab4
Stefan Prisca <stefan.prisca@gmail.com>
2018-01-13 18:24:10 +0100

    Move back to local server
    Signed-off-by: Stefan Prisca <stefan.prisca@gmail.com>

--- github.stefanprisca.bbvoice/src/web/main.js
+++ github.stefanprisca.bbvoice/src/web/main.js
@@ -116,7 +116,7 @@ var room = 'foo';
 // Could prompt for room name:
 // room = prompt('Enter room name:');
 
-var socket = new WebSocket('wss://52.28.20.194:443/websocket?id='+socketId)
+var socket = new WebSocket('wss://127.0.0.1:8124/websocket?id='+socketId)
 
 function sendMessage(message, destination = "", treeId = "") {
   console.log('Client sending message: ', message);
